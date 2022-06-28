fc97eeed06429707d3b320ac07e1a23d0a2b4d9d
Alan Liu <alan.liu@ccc.govt.nz>
2022-06-28 21:58:31 +1200

    First commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Alan Liu on 28/06/22.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
